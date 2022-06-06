ee29bf21272cdec757615059b1a920efd9f4fada
MissDine <enidnyatichi1212@gmail.com>
2022-06-06 11:59:33 +0300

    Feat:Edit

--- lib/Errors.rb
+++ lib/Errors.rb
@@ -1,1 +1,2 @@
 # FOUR COMMON ERROR TYPES IN RUBY
+# Name errors-occur when a name is invalid or undefined-ruby assumes that this name is a name of a value or method hence if its not defined it gives a name error.
